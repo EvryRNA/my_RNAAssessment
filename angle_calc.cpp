7299970d6b47caa89c33f45089a704e9a199f47a
FranGASTRIN <gastrin.francois@gmail.com>
2022-02-16 18:13:52 +0100

    Update 2022-02-16

--- angle_calc.cpp
+++ angle_calc.cpp
@@ -12,6 +12,7 @@ using namespace std;
 // Store the coordinates of the atoms we are interested in for 1 PDB file
 vector<vector<float> > coord_pdb(string pdbfile){
 	vector<vector<float> > tableau;
+	vector<string> tab_atom;
 	ifstream fl(pdbfile);
 	string line;
 	const vector<string> refer = {"N ","CA","C "};
@@ -19,16 +20,22 @@ vector<vector<float> > coord_pdb(string pdbfile){
 
 	while(getline(fl, line)){
 		if (line.substr(0,4) == "ATOM"){
-			if (*find(refer.begin(), refer.end(), line.substr(13,2)) == line.substr(13,2))
+			tab_atom.push_back(line.substr(13,2));
+			if (tab_atom.front() != refer.front())
 			{
-				float X = stof(line.substr(30,8));  //
-				float Y = stof(line.substr(38,8));  // Convert string to float
-				float Z = stof(line.substr(46,8));  //
-				tableau.push_back(vector<float>(3,0.0));
-				i += 1;
-				tableau[i][0] = X;  //
-				tableau[i][1] = Y;  // Atomic coordinates x, y, z
-				tableau[i][2] = Z;  //
+				tab_atom.pop_back();
+			} else {
+				if (*find(refer.begin(), refer.end(), line.substr(13,2)) == line.substr(13,2))
+				{
+					float X = stof(line.substr(30,8));  //
+					float Y = stof(line.substr(38,8));  // Convert string to float
+					float Z = stof(line.substr(46,8));  //
+					tableau.push_back(vector<float>(3,0.0));
+					i += 1;
+					tableau[i][0] = X;  //
+					tableau[i][1] = Y;  // Atomic coordinates x, y, z
+					tableau[i][2] = Z;  //
+				}
 			}		
 		}
 	}
@@ -75,67 +82,47 @@ float torsion_angle(vector<float> atom1, vector<float> atom2, vector<float> atom
     vector<float> vecteur_normal1 = vector_prod(vecteur12, vecteur23);
     vector<float> vecteur_normal2 = vector_prod(vecteur23, vecteur34);
 
-    float angle = sin(scalar_prod(vecteur_normal1,vecteur_normal2)/(norme(vecteur_normal1)
-    			*norme(vecteur_normal2)))*180;
-    return angle;
+    if (scalar_prod(vecteur23,vector_prod(vecteur_normal1,vecteur_normal2))<0)
+    {
+	    float angle = acos(scalar_prod(vecteur_normal1,vecteur_normal2)/
+	    	          (norme(vecteur_normal1)*norme(vecteur_normal2)))*180/M_PI;
+	    return angle;
+    } else {
+    	float angle = -acos(scalar_prod(vecteur_normal1,vecteur_normal2)/
+	    	          (norme(vecteur_normal1)*norme(vecteur_normal2)))*180/M_PI;
+    	return angle;
+    }   
 }
 
 
 int main(int argc, char** argv)
 {
-	string in_dir = argv[1];			// Pathway of the repository
+	string in_dir = argv[1];      // Pathway of the repository
 
-// Stokage of the file names of the directory given in input
-	DIR *dir; 				// Pointeur du répertoire, fonctionne avec readdirr (package <dirent.h>)
-	struct dirent *diread;	// Accès aux noms des fichiers dans le répertoire par la structure dirent (package <dirent.h>)
-    vector<char *> files;								//
-	if ((dir = opendir(argv[1])) != nullptr) {			//
-		while ((diread = readdir(dir)) != nullptr) {	// Listes des fichiers dans le répertoire
-	               files.push_back(diread->d_name);		//
-		}
-		closedir (dir);
-	} else {
-		perror ("opendir");
-		        return EXIT_FAILURE;
-	}
+	/* Processing for each file in PDB files list */
 
-// File processing if it is a PDB file
-	ofstream flo;
-	flo.open("liste_pdb.txt"); // Open a new file to stock all the PDB file names in the repository
-	if (flo.is_open())
-	{
-		for (auto file : files)
-		{
-			int lenf = strlen(file);
-			string fl = file;
-			if ((lenf > 5) && (fl.substr(lenf-4,4) == ".pdb"))
-			{
-				flo << file << endl;
-			}
-		}
-	}
-	flo.close();
-
-	ifstream my_pdb("liste_pdb.txt");
+	ifstream my_pdbs(argv[2]);    // File containing PDB files list
 	string line;
-	while(getline(my_pdb, line)) // 1 line = 1 PDB file
+	while(getline(my_pdbs, line)) // 1 line = 1 PDB file
 	{
 		string fl = line;
 		cout << fl << endl;
 		vector<vector<float> > Coords;
-		string namef = argv[2];
+		string namef = argv[3];
 		string ffile = namef+"_"+fl.substr(0,fl.size()-4)+".txt";  // Output file name + processed PDB code
 		
 		ofstream file_out;
 
 		Coords = coord_pdb(in_dir+fl);
-		file_out.open(ffile); // Open a new file
+		file_out.open(ffile);           // Open a new file for angle values
 		if (file_out.is_open())
 		{
-			for (int i = 1; i < Coords.size()-4; i += 4)
+			for (int i = 0; i < Coords.size()-6; i += 4)
 			{
-				float angle = torsion_angle(Coords[i], Coords[i+1], Coords[i+2], Coords[i+3]);
-				file_out << angle << endl;
+				int j = i+2;
+				float angle_psi = torsion_angle(Coords[i], Coords[i+1], Coords[i+2], Coords[i+3]);
+				float angle_phi = torsion_angle(Coords[j], Coords[j+1], Coords[j+2], Coords[j+3]);
+				file_out << angle_psi << "   " << angle_phi << endl;
 			}
 			cout << "Done !" << endl;
 			file_out.close();
