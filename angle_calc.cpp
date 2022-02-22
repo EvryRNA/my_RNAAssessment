05fc03e2bd646445b7d35db02cfcd16c0e373d59
FranGASTRIN <gastrin.francois@gmail.com>
2022-02-22 18:53:31 +0100

    Update 2022-02-22

--- angle_calc.cpp
+++ angle_calc.cpp
@@ -9,38 +9,63 @@
 
 using namespace std;
 
-// Store the coordinates of the atoms we are interested in for 1 PDB file
-vector<vector<float> > coord_pdb(string pdbfile){
-	vector<vector<float> > tableau;
+/* Store the coordinates of the atoms we are interested in for 1 PDB file */
+vector<vector<vector<float> >> coord_pdb(string pdbfile){
+	vector<vector<vector<float> >> tableau;         // Atomic coordinates for all chains
+	vector<vector<float> > interm_tab;              // Atomic coordinates for current chain
 	vector<string> tab_atom;
-	ifstream fl(pdbfile);
+	vector<string> tab_chain = {"A"};               // To now in which chain we are currently (start with "A")
 	string line;
 	string Atom;
-	const vector<string> refer = {"N ","CA","C "};
+	const vector<string> refer = {"N ","CA","C "};  // Atoms that interest us
 	int i = -1;
 
+	ifstream fl(pdbfile);
 	while(getline(fl, line)){
 		if (line.substr(0,4) == "ATOM"){
 			Atom = line.substr(13,2);
 			tab_atom.push_back(Atom);               //
 			if (tab_atom.front() != refer.front())  // Avoids possible mismatches if the N-ter part
 			{                                       // is missing from the PDB file 
-				tab_atom.pop_back();            //
+				tab_atom.pop_back();                //
 			} else {
-				if (*find(refer.begin(), refer.end(), Atom) == Atom)
+				if (tab_chain.back() == line.substr(21,1))  // If still in the same chain
 				{
-					float X = stof(line.substr(30,8));  //
-					float Y = stof(line.substr(38,8));  // Convert string to float
-					float Z = stof(line.substr(46,8));  //
-					tableau.push_back(vector<float>(3,0.0));
-					i += 1;
-					tableau[i][0] = X;  //
-					tableau[i][1] = Y;  // Atomic coordinates x, y, z
-					tableau[i][2] = Z;  //
+					if (*find(refer.begin(), refer.end(), Atom) == Atom)
+					{
+						float X = stof(line.substr(30,8));  //
+						float Y = stof(line.substr(38,8));  // Convert string to float
+						float Z = stof(line.substr(46,8));  //
+						interm_tab.push_back(vector<float>(3,0.0));
+						i += 1;
+						interm_tab[i][0] = X;  //
+						interm_tab[i][1] = Y;  // Atomic coordinates x, y, z
+						interm_tab[i][2] = Z;  //
+					}
+				}else {
+					tab_chain.push_back(line.substr(21,1));       // Chain changing
+					if (!interm_tab.empty())              //
+					{                                     // Avoid to stock an empty vector if the 
+						tableau.push_back(interm_tab);    // first chain is not "A"
+						interm_tab.clear();               //
+					}
+					i = -1;
+					if (*find(refer.begin(), refer.end(), Atom) == Atom)
+					{
+						float X = stof(line.substr(30,8));  //
+						float Y = stof(line.substr(38,8));  // Convert string to float
+						float Z = stof(line.substr(46,8));  //
+						interm_tab.push_back(vector<float>(3,0.0));
+						i += 1;
+						interm_tab[i][0] = X;  //
+						interm_tab[i][1] = Y;  // Atomic coordinates x, y, z
+						interm_tab[i][2] = Z;  //
+					}
 				}
 			}		
 		}
 	}
+	tableau.push_back(interm_tab);    // Stock atomic coordinates for the last chain
 	return tableau;
 }
 
@@ -51,6 +76,7 @@ float distance(vector<float> atom1, vector<float> atom2){
 }
 
 
+// Norme of a vector
 float norme(vector<float> vecteur){
 	vector<float> Vec0 = {0.0, 0.0, 0.0};
 	float D = distance(vecteur, Vec0);
@@ -84,7 +110,7 @@ float torsion_angle(vector<float> atom1, vector<float> atom2, vector<float> atom
     vector<float> vecteur_normal1 = vector_prod(vecteur12, vecteur23);
     vector<float> vecteur_normal2 = vector_prod(vecteur23, vecteur34);
 
-    if (scalar_prod(vecteur23,vector_prod(vecteur_normal1,vecteur_normal2))<0)
+    if (scalar_prod(vecteur23,vector_prod(vecteur_normal1,vecteur_normal2))<0)  // To know if value will be '-' or '+'
     {
 	    float angle = acos(scalar_prod(vecteur_normal1,vecteur_normal2)/
 	    	          (norme(vecteur_normal1)*norme(vecteur_normal2)))*180/M_PI;
@@ -109,26 +135,31 @@ int main(int argc, char** argv)
 	{
 		string fl = line;
 		cout << fl << endl;
-		vector<vector<float> > Coords;
-		string namef = argv[3];         // Output file name without extension (".txt",".out",etc...)
+		vector<vector<vector<float> >> Coords;
+		string namef = argv[3];                   // Output file name without extension (".txt",".out",etc...)
 		string ffile = namef+"_"+fl.substr(0,fl.size()-4)+".txt";  // Output file name + processed PDB code
 		
 		ofstream file_out;
 
 		Coords = coord_pdb(in_dir+fl);
+
 		file_out.open(ffile);           // Open a new file for angle values
 		if (file_out.is_open())
 		{
-			for (int i = 0; i < Coords.size()-6; i += 4)
+			for (int k = 0; k < Coords.size(); k++)
 			{
-				int j = i+2;
-				float angle_psi = torsion_angle(Coords[i], Coords[i+1], Coords[i+2], Coords[i+3]);
-				float angle_phi = torsion_angle(Coords[j], Coords[j+1], Coords[j+2], Coords[j+3]);
-				file_out << angle_psi << "   " << angle_phi << endl;
+		        file_out << "Chain " << k+1 << endl;
+				for (int i = 0; i < Coords[k].size()-6; i+=3)
+				{
+					int j = i+2;
+					float angle_psi = torsion_angle(Coords[k][i], Coords[k][i+1], Coords[k][i+2], Coords[k][i+3]);
+					float angle_phi = torsion_angle(Coords[k][j], Coords[k][j+1], Coords[k][j+2], Coords[k][j+3]);
+					file_out << angle_psi << "   " << angle_phi << endl;
+				}
 			}
-			cout << "Done !" << endl;
-			file_out.close();
 		}
+	cout << "Done !" << endl;
+	file_out.close();
 	}
 	return 0;
 }
