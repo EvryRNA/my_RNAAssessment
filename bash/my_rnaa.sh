1520f97d603a2153ccc987a35b43dcd73f36a2e3
FranGASTRIN <gastrin.francois@gmail.com>
2022-06-15 17:09:58 +0200

    Script bash

--- bash/my_rnaa.sh
+++ bash/my_rnaa.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ $5 = "rna" ];then
+if [ "$5" = "rna" ];then
 	echo "Build trainset"
 	./../distance_calculation -d $1 -l $2 -R -o Distance_trainset
 	echo "\nBuild testset"
@@ -12,7 +12,11 @@ if [ $5 = "rna" ];then
 	python3 ../src/Preprocessing.py -D Distance_testset.txt -o rna_pp_test -rna -no_mean_struct
 
 	echo "\nMachine learning training and scoring"
-	python3 ../src/ML_scripts/ML_CNN.py -D rna_pp_train.npz -pred rna_pp_test.npz
+	if [ -z $6 ];then
+		python3 ../src/ML_scripts/ML_CNN.py -D rna_pp_train.npz -pred rna_pp_test.npz
+	else
+		python3 ../src/ML_scripts/$6 -D rna_pp_train.npz -pred rna_pp_test.npz
+	fi
 else
 	echo "Build trainset"
 	./../distance_calculation -d $1 -l $2 -o Distance_trainset
@@ -25,5 +29,9 @@ else
 	python3 ../src/Preprocessing.py -D Distance_testset.txt -o prot_pp_test -no_mean_struct
 
 	echo "\nMachine learning training and scoring"
-	python3 ../src/ML_scripts/ML_CNN.py -D prot_pp_train.npz -pred prot_pp_test.npz
+	if [ -z $5 ];then
+		python3 ../src/ML_scripts/ML_CNN.py -D prot_pp_train.npz -pred prot_pp_test.npz
+	else
+		python3 ../src/ML_scripts/$5 -D prot_pp_train.npz -pred prot_pp_test.npz
+	fi
 fi	
